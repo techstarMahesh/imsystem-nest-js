cfd7a4750a9919016aaaa7863ea061e1b10b5d83
techstarmahesh <mistermaheshsharma@gmail.com>
2023-12-22 21:42:01 +0530

    database connection is successfully

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,22 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import databaseConfig from './database/databaseConfig';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { dbConfig } from './database/dbConfig';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ load: [databaseConfig], isGlobal: true, cache: true }),
-    TypeOrmModule.forRootAsync({ databaseConfig }),
+    // TypeOrmModule.forRootAsync({ ...dbConfig }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DATABASE_HOST,
+      port: Number(process.env.DATABASE_PORT),
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      autoLoadEntities: true,
+      synchronize: Boolean(process.env.DATABASE_SYNCHRONIZE),
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
